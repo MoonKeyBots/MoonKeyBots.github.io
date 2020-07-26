71087594261a917dcbe2c1f5fd6ad76d95ff06f5
MoonKeyBots <68734599+MoonKeyBots@users.noreply.github.com>
2020-07-26 23:37:43 +0800

    Update script.js

--- md5_generator/js/script.js
+++ md5_generator/js/script.js
@@ -1,6 +1,10 @@
+document.getElementById('input').value = document.location.search;
+var text = document.getElementById('input').value;
+document.getElementById('output').value = md5(text);
 var input = document.getElementById('input');
 input.oninput = function() {
 	var text = document.getElementById('input').value;
 	document.getElementById('output').value = md5(text);
 };
+
 
